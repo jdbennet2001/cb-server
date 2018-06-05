d6b6d963dd0b4c118722291438c74294d1856269
Jon Bennett <jdbennet@ca.ibm.com>
2018-06-05 18:08:23 -0400

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha" : true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+
+        "no-console": 0,
+        "no-mixed-spaces-and-tabs" : 0
+    }
+};
