7d402dbde37d682916947f83f72db2ba6b2c26d1
Jon Bennett <jdbennet@ca.ibm.com>
2018-06-26 14:30:16 -0400

    Archive conversion CBR -> CBZ

--- batch/convert-rar.js
+++ batch/convert-rar.js
@@ -1,17 +1,71 @@
 const path      = require('path');
+const fs        = require('fs');
+const _         = require('lodash');
 
-const input     = path.join(__dirname, "../tests/archives/comics/Chew/Chew #005 (2009-11-27) - Taster's Choice, Part. 5 of 5.cbr");
 const tmp_dir   = path.join(__dirname, './tmp');
-const output    = path.join(__dirname, './out.zip');
 
 
 const {convert}     = require('../lib/convert');
 
-debugger;
+const {files} = scan_directory('/Volumes/VIDEO/comics');
 
-convert(input, output, tmp_dir).then( () =>{
-    console.log('converted');
-}, err => {
-    console.error(`conversion error: ${err.message}`)
+const archives = files.filter(file =>{
+    return _.endsWith(file, 'cbr');
 })
 
+console.log(`${_.size(archives)} found`);
+
+let conversion = _.reduce(archives, function(promise_chain, current_task, index) {
+    return promise_chain.then( () =>{
+        return convert(current_task, tmp_dir);
+    });
+}, Promise.resolve() );
+
+conversion.then( () =>{
+    console.log(`Complete, ${_.size(archives)} converted.`)
+}, err =>{
+    console.error(`Error in conversion process ${err.message}`);
+})
+
+/*
+ Return fully qualified paths for all  useful files in a directory
+ {
+    files: []
+    folders: []
+ }
+ */
+function scan_directory(directory){
+
+    let catalog_model = fs.readdirSync( directory );
+
+    console.log(`Scanning folder: ${directory}`);
+
+    //Remove system generated directories / thumbnails
+    catalog_model = catalog_model.filter(entry => {
+        return !_.startsWith(entry,'.') && !_.startsWith(entry, '_');
+    })
+
+    //Map basename to fully qualified path names
+    catalog_model = catalog_model.map( entry => {
+        return path.join(directory, entry);
+    })
+
+    //Seperate out files and folders
+    let files = catalog_model.filter(entry => {
+            return fs.statSync(entry).isFile();
+    })
+
+
+    let folders = catalog_model.filter(entry => {
+            return fs.statSync(entry).isDirectory();
+    })
+
+    _.forEach(folders, folder =>{
+        let results = scan_directory(folder);
+        files = _.concat(files, results.files);
+        folders = _.concat(folders, results.folders);
+    })
+
+    return {files, folders};
+
+}
