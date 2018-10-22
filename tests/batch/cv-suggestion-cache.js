42f183cb75cd6e04684368fe12ae54d0b46978ea
Jonathan Bennett <jdbennet@ca.ibm.com>
2018-10-21 21:13:51 -0400

    More issue data

--- tests/batch/cv-suggestion-cache.js
+++ tests/batch/cv-suggestion-cache.js
@@ -9,8 +9,8 @@ describe('Suggestions', function() {
 
 	it('..can pre-populate the cache', async () => {
 
-		for ( let year = 2018; year > 2010; year--){
-			for (let issue = 1; issue < 26; issue++){
+		for ( let year = 2018; year > 2008; year--){
+			for (let issue = 1; issue < 52; issue++){
 				let results = await data(issue, year);
 				console.log( `Total cached: ${_.size(results)} results.`);
 			}
